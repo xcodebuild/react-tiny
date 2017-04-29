724e98c08eaa3ea0e8aa957bc31dbb59a3d42143
codefalling <code.falling@gmail.com>
2017-04-29 17:29:25 +0800

    tsx works

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
-export function render(element: string, container: HTMLElement) {
+declare global {
+  namespace JSX {
+    type Element = TReactElement | string | number | null;
+    interface IntrinsicElements { [elemName: string]: any; } 
+  }
+}
+
+export function render(
+	element: JSX.Element, 
+	container: HTMLElement
+): void {
 	const wrapper = document.createElement('span');
-	wrapper.innerText = element;
+	if (typeof element === 'string') {
+		wrapper.innerText = element;
+	}
 	container.appendChild(wrapper);
+}
+
+class TReactElement {
+	type: string | Function;
+	props: Object;
+	constructor(
+		type: string | Function,
+		attrs: Object | null,
+		children: Array<JSX.Element> | null,
+	) {
+		this.type = type;
+	}
+}
+
+export function h(
+	type: string | Function | null, 
+	attrs: Object | null, 
+	...children: Array<JSX.Element>,
+): JSX.Element {
+	if (type === null) return null;
+	return new TReactElement(type, attrs, children);
 }
--- tests/render-spec.tsx
+++ tests/render-spec.tsx
@@ -1,18 +1,23 @@
 import {createDiv} from './utils';
 
-import {render} from '../src/index';
+import {render, h} from '../src/index';
 
 declare const describe: Function;
 declare const it: Function;
 declare const expect: Function;
 
-describe('#render', function () {
-    it('render "hello" should got <span>hello</span>', function () {
+describe('#render', () => {
+    it('render "hello" should got <span>hello</span>', () => {
     	const container: HTMLElement = createDiv();
     	render('hello', container);
         const target: Element | null = container.children[0];
         expect(target).to.not.be.undefined;
         expect(target.tagName.toLowerCase()).to.be.equal('span');
         expect(target.innerHTML).to.be.equal('hello');
+    });
+
+    it('render "<div>test</div> should got html itself', () => {
+        const container: HTMLElement = createDiv();
+        render(<div>test</div>, container);
     });
 });
